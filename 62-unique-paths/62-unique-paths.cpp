2bf09d834175eb4b0ed54c05f5d7fcd37fc3a12f
Shirish Sharma <59448012+Shirish2@users.noreply.github.com>
2022-08-10 11:52:13 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.61%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int a(int i,int j,vector<vector<int>> &dp, int &m,int &n){
+        if( i==m-1 || j==n-1){
+            return 1;
+        }
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+        dp[i][j]=a(i+1,j,dp,m,n)+a(i,j+1,dp,m,n);
+        return dp[i][j];
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        return a(0,0,dp,m,n);
+
+    }
+};
