35eec048b49f230f42af45e3b9f9e48eea62aa88
Shirish Sharma <59448012+Shirish2@users.noreply.github.com>
2022-08-10 12:01:19 +0530

    Time: 25 ms (73.49%), Space: 19.5 MB (92.27%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority;
+        int occur=0;
+        for(int i=0;i<nums.size();i++){
+            if(occur==0){
+                majority=nums[i];
+                occur++;
+            }
+            else if(nums[i]==majority){
+                occur++;
+            }
+            else{
+                occur--;
+            }
+        }
+        return majority;
+        
+    }
+};
